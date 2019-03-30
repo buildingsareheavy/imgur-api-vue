7b8303d97d50b2711389cc5175401bc52f269149
Ethan Barrett <hello@buildingsareheavy.com>
2019-03-29 23:24:28 -0600

    AuthHandler and Vue Router is setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
 import store from "./store"; // store/index.js
+import AuthHandler from "./components/AuthHandler.vue";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [{ path: "/oauth2/callback", component: AuthHandler }]
+});
+
 new Vue({
+  router,
   store, // shorthand for => store: store
   render: h => h(App)
 }).$mount("#app");
